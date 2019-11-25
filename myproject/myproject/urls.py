1ff3f035f597dd2f29f1c7674de5188818bd5b8a
Aggie Wheeler Bateman <48076903+aggiewb@users.noreply.github.com>
2019-11-25 11:00:41 -0800

    Update urls.py

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import path # not in the book
+from django.conf.urls import url # not in the book
 from django.contrib import admin
-
 from django.urls import path, include # new
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     # path('admin/', admin.site.urls), # from the book
     path(r'^myapp/', include('myapp.urls')), # new
 ]
